7eed8849cec27d9603d1342fa06c5135975a4a13
NullNUMMER24 <j.rohrbach@sensemail.ch>
2023-09-12 16:59:14 +0200

    API changed to Berne

--- main.go
+++ main.go
@@ -1,25 +1,67 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "log"
-  "net/http"
-  "os"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
-func main()  {
-  response, err := http.Get("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=bb7302fe136e4097821c1f74e3a51765")
+type Weather struct {
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Dt  int `json:"dt"`
+	Sys struct {
+		Type    int    `json:"type"`
+		ID      int    `json:"id"`
+		Country string `json:"country"`
+		Sunrise int    `json:"sunrise"`
+		Sunset  int    `json:"sunset"`
+	} `json:"sys"`
+	Timezone int    `json:"timezone"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Cod      int    `json:"cod"`
+}
 
-  if err != nil {
-    fmt.Print(err.Error())
-    os.Exit(1)
-  }
+func main() {
+	var weather Weather
+	response, err := http.Get("https://api.openweathermap.org/data/2.5/weather?lat=46.94&lon=7.40&appid=bb7302fe136e4097821c1f74e3a51765")
 
-  responseData, err := ioutil.ReadAll(response.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
-  fmt.Println(string(responseData))
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
 
+	if responseData, err := io.ReadAll(response.Body); err != nil {
+		fmt.Println(err)
+	} else {
+		json.Unmarshal(responseData, &weather)
+	}
+	fmt.Printf("The current temperature in %s is %.2f°C.\n", weather.Name, weather.Main.Temp-273.15)
 }
